8b5d4f12501601d9dfe3e94d16e4e0bc08ffc88b
Stepan Morozov <morsstepan@icloud.com>
2020-02-26 12:46:41 +0300

    Created Spring Boot project

---
+++ src/main/java/com/morsstepan/musictastecover/MusicTasteCoverApplication.java
@@ -0,0 +1,13 @@
+package com.morsstepan.musictastecover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTasteCoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicTasteCoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morsstepan/musictastecover/MusicTasteCoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morsstepan.musictastecover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTasteCoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
