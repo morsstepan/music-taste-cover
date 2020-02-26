87451327864e8edc8ac909fc7978df02e0e86076
Stepan Morozov <morsstepan@icloud.com>
2020-02-26 13:41:22 +0300

    Added playground and test resources

---
+++ src/test/java/com/morsstepan/musictastecover/ImagePlayground.java
@@ -0,0 +1,36 @@
+package com.morsstepan.musictastecover;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class ImagePlayground {
+
+    @Test
+    public void mergeImages() {
+
+        BufferedImage img1;
+
+    }
+
+    public static BufferedImage joinBufferedImage(BufferedImage img1, BufferedImage img2) {
+        //do some calculate first
+        int offset  = 5;
+        int wid = img1.getWidth()+img2.getWidth()+offset;
+        int height = Math.max(img1.getHeight(),img2.getHeight())+offset;
+        //create a new buffer and draw two image into the new image
+        BufferedImage newImage = new BufferedImage(wid,height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = newImage.createGraphics();
+        Color oldColor = g2.getColor();
+        //fill background
+        g2.setPaint(Color.WHITE);
+        g2.fillRect(0, 0, wid, height);
+        //draw image
+        g2.setColor(oldColor);
+        g2.drawImage(img1, null, 0, 0);
+        g2.drawImage(img2, null, img1.getWidth()+offset, 0);
+        g2.dispose();
+        return newImage;
+    }
+}
